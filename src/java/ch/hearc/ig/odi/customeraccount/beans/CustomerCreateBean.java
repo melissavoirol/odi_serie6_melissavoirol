364da0564034feee1424a7927fe4d73249a141bf
Melissa Voirol <melissa.voirol@he-arc.ch>
2016-04-21 21:46:30 +0200

    Modified createCustomer and createCustomerOk
    Série 8 : Faire le lien entre createCustomer et createCustomerOk !
    + requiredMessage sur inputText qui ne s'affichent pas correctement...

--- src/java/ch/hearc/ig/odi/customeraccount/beans/CustomerCreateBean.java
+++ src/java/ch/hearc/ig/odi/customeraccount/beans/CustomerCreateBean.java
@@ -1,20 +1,57 @@
 package ch.hearc.ig.odi.customeraccount.beans;
 
+import ch.hearc.ig.odi.customeraccount.services.Services;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 /**
  *
  * @author Melissa Voirol <melissa.voirol@he-arc.ch>
  */
-@Named(value = "CustomerCreateBean")
+@Named(value = "customerCreateBean")
 @RequestScoped
 public class CustomerCreateBean {
+    
+    @Inject
+    Services services;
+    
+    private int number;
+    private String firstName;
+    private String lastName;
 
     /**
      * Creates a new instance of CustomerCreateBean
      */
     public CustomerCreateBean() {
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    public void save() {
+        services.saveCustomer(number, firstName, lastName);
     }
     
 }
--- src/java/ch/hearc/ig/odi/customeraccount/beans/CustomersBean.java
+++ src/java/ch/hearc/ig/odi/customeraccount/beans/CustomersBean.java
@@ -4,7 +4,7 @@ import ch.hearc.ig.odi.customeraccount.business.Customer;
 import ch.hearc.ig.odi.customeraccount.services.Services;
 import java.io.Serializable;
 import java.util.List;
-import javax.enterprise.context.SessionScoped;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -13,7 +13,7 @@ import javax.inject.Named;
  * @author Melissa Voirol <melissa.voirol@he-arc.ch>
  */
 @Named(value = "customersBean")
-@SessionScoped
+@RequestScoped
 public class CustomersBean implements Serializable {
 
     @Inject
